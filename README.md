fa05a1acd2656512518abe52544357f7561acad6
Haksell <axel.brisse@gmail.com>
2025-03-21 02:55:48 +0100

    screenshot

---
+++ README.md
@@ -0,0 +1,19 @@
+# scop
+
+## todo
+
+- https://vulkan-tutorial.com/
+- remove `VulkanApp`, functions are fine
+- refactorrrrrrrrrrrrr
+- update dependencies
+- parse .obj files myself
+- remove non-ash/winit dependencies
+- finish scop
+- add bonuses (125)
+
+## bonus
+
+- [ ] backface culling
+- [ ] phong model
+- [ ] platonic solid objects
+- [ ] light objects
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,48 @@
+use winit::{
+    event::{ElementState, Event, KeyboardInput, VirtualKeyCode, WindowEvent},
+    event_loop::{ControlFlow, EventLoop},
+};
+
+const WINDOW_WIDTH: u32 = 800;
+const WINDOW_HEIGHT: u32 = 600;
+
+struct VulkanApp;
+
+impl VulkanApp {
+    fn init_window(event_loop: &EventLoop<()>) -> winit::window::Window {
+        winit::window::WindowBuilder::new()
+            .with_title("scop")
+            .with_inner_size(winit::dpi::LogicalSize::new(WINDOW_WIDTH, WINDOW_HEIGHT))
+            .build(event_loop)
+            .unwrap()
+    }
+
+    pub fn main_loop(event_loop: EventLoop<()>) {
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent { event, .. } => match event {
+                WindowEvent::CloseRequested => *control_flow = ControlFlow::Exit,
+                WindowEvent::KeyboardInput { input, .. } => match input {
+                    KeyboardInput {
+                        virtual_keycode,
+                        state,
+                        ..
+                    } => match (virtual_keycode, state) {
+                        (Some(VirtualKeyCode::Escape), ElementState::Pressed) => {
+                            dbg!();
+                            *control_flow = ControlFlow::Exit
+                        }
+                        _ => {}
+                    },
+                },
+                _ => {}
+            },
+            _ => (),
+        })
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let event_loop = EventLoop::new();
+    let _window = VulkanApp::init_window(&event_loop);
+    VulkanApp::main_loop(event_loop);
 }
